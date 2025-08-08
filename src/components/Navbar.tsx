2ca41abf209cda8cfeffe1bb3eb9f64e3455577f
dieKrake <kkrazius@gmail.com>
2025-08-08 20:20:38 +0200

    Add navigation-menu from shadcn ui
    better look and less work, NavUser is still used.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,42 @@
-import NavLinks from "./NavLinks";
+// src/components/Navbar.tsx
+
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import NavUser from "./NavUser";
 
 export default function Navbar() {
   return (
-    <nav className="absolute top-0 w-full">
-      <div className="flex flex-row gap-8 border-4 border-amber-200 h-16 w-full justify-between">
-        <NavLinks />
+    <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              {/* NEUE, MODERNE STRUKTUR */}
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/">Startseite</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              {/* NEUE, MODERNE STRUKTUR */}
+              <NavigationMenuLink
+                asChild
+                className={navigationMenuTriggerStyle()}
+              >
+                <Link href="/calendar">Kalender</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
         <NavUser />
       </div>
     </nav>
